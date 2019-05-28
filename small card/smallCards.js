9d3f2b325033cf8f35b76427da9eaa9db4ecc06e
Anette Vestvik <Anette_vestvik@hotmail.com>
2019-05-28 13:43:02 +0200

    Small cards html and css,
    må skrives om til JavaScript kode

---
+++ small card/smallCards.js
@@ -0,0 +1,9 @@
+
+/*Sette forfallsdato i kortet*/
+function myFunction() {
+  var x = document.getElementById("myDate").value;
+  document.getElementById("demo").innerHTML = x;
+}
+
+// Get the modal
+var modal = document.getElementById('dd');
